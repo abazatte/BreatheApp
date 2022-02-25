d01fb79d606fd8b625b5488591241f9403ca13ff
abazatte <abdurrahman.azattemuer@hs-osnabrueck.de>
2022-02-25 16:08:29 +0100

    Ja die Fragmente

--- app/src/main/java/com/example/datenbankefuerprojekt/db/main/database/UebungDatenbank.java
+++ app/src/main/java/com/example/datenbankefuerprojekt/db/main/database/UebungDatenbank.java
@@ -9,8 +9,11 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import java.sql.Date;
+import java.util.Random;
 
-@Database(entities = {Uebung.class, Fragment.class, ControlPause.class}, version = 10)
+
+@Database(entities = {Uebung.class, Fragment.class, ControlPause.class}, version = 11)
 public abstract class UebungDatenbank extends RoomDatabase {
     private static UebungDatenbank INSTANCE;
     public abstract UebungDao uebungDao();
@@ -38,9 +41,13 @@ public abstract class UebungDatenbank extends RoomDatabase {
 
     private static class PopulateDbAsyncTask extends AsyncTask<Void,Void,Void> {
         private UebungDao uebungDao;
+        private ControlPauseDao controlPauseDao;
+        private FragmentDao fragmentDao;
 
         private PopulateDbAsyncTask(UebungDatenbank database){
             this.uebungDao = database.uebungDao();
+            this.controlPauseDao = database.controlPauseDao();
+            this.fragmentDao = database.fragmentDao();
         }
 
         @Override
@@ -48,7 +55,28 @@ public abstract class UebungDatenbank extends RoomDatabase {
             uebungDao.insert(new Uebung("Title 1", "Description 1", 1,1,false,0,1));
             uebungDao.insert(new Uebung("Title 2", "Description 2", 1,1,false,0,1));
             uebungDao.insert(new Uebung("Title 3", "Description 3", 1,1,false,0,1));
+
+            for (int i = 1; i <= 3; i++) {
+                fragmentDao.insert(new Fragment("Fragment 1", i, 2,3,4,5,2,4));
+                fragmentDao.insert(new Fragment("Fragment 2", i, 3,4,4,5,2,3));
+            }
+
+            final int MAX_Y_VALUE = 50;
+            final int MIN_Y_VALUE = 5;
+            float y;
+            for (int i = 0; i < 12; i++) {
+                y = MIN_Y_VALUE + new Random().nextFloat() * (MAX_Y_VALUE - MIN_Y_VALUE);
+                Date date = new Date(2022, i, 12);
+                ControlPause controlPause = new ControlPause(date, (long) y);
+                y = MIN_Y_VALUE + new Random().nextFloat() * (MAX_Y_VALUE - MIN_Y_VALUE);
+                Date date1 = new Date(2022, i, 25);
+                ControlPause controlPause1 = new ControlPause(date1, (long) y);
+                controlPauseDao.insert(controlPause);
+                controlPauseDao.insert(controlPause1);
+            }
             return null;
         }
     }
+
+
 }
