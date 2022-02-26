d812d6d03e40c1ccef2ed862add90ddf95ec3106
maxono <maximilian.jaesch@hs-osnabrueck.de>
2022-02-26 16:55:39 +0100

    jaaaAAAAAAAaaaaa

--- app/src/main/java/com/example/datenbankefuerprojekt/animation/AnimationFragment.java
+++ app/src/main/java/com/example/datenbankefuerprojekt/animation/AnimationFragment.java
@@ -1,6 +1,5 @@
 package com.example.datenbankefuerprojekt.animation;
 
-import static com.example.datenbankefuerprojekt.animation.AnimationViewModel.BASE_ANIMATION_DURATION;
 import static com.example.datenbankefuerprojekt.ui.home.HomeFragment.EXTRA_COUNT;
 import static com.example.datenbankefuerprojekt.ui.home.HomeFragment.EXTRA_DESC;
 import static com.example.datenbankefuerprojekt.ui.home.HomeFragment.EXTRA_ID;
@@ -13,16 +12,20 @@ import static com.example.datenbankefuerprojekt.ui.home.HomeFragment.EXTRA_USE_S
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
@@ -32,8 +35,10 @@ import com.example.datenbankefuerprojekt.db.main.database.uebung.Uebung;
 import com.example.datenbankefuerprojekt.ui.home.HomeViewModel;
 
 import java.util.List;
+import java.util.Locale;
 
 public class AnimationFragment extends Fragment {
+    public static final String TAG = "AnimationFragment";
 
     private HomeViewModel homeViewModel;
 
@@ -41,9 +46,14 @@ public class AnimationFragment extends Fragment {
     private AnimationFragmentBinding binding;
 
     private LottieAnimationView lottieAnimationView;
-    private Button startButton;
-    private Button stopButton;
+    private Button buttonStartPause;
     private Button resetButton;
+
+    private TextView textViewCountDown;
+    private TextView textViewTitle;
+    private TextView textViewFragmentTitle;
+
+    private boolean alreadyInitialised;
     //Textview timer??
 
     /*TODO:
@@ -69,10 +79,12 @@ public class AnimationFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         //this viewmodel should be owned by "this" so it gets destroyed when we have a different uebung
         mViewModel = new ViewModelProvider(this).get(AnimationViewModel.class);
+
         homeViewModel = new ViewModelProvider(requireActivity()).get(HomeViewModel.class);
         binding = AnimationFragmentBinding.inflate(inflater,container,false);
         View root = binding.getRoot();
 
+        Log.i(TAG, "onCreateView: wird doppler?");
         initMemberVariables();
 
         //das kann nicht null sein idk
@@ -84,8 +96,13 @@ public class AnimationFragment extends Fragment {
         receivedUebung.setId(bundle.getInt(EXTRA_ID, 0));
         mViewModel.setCurrentUebung(receivedUebung);
 
-        //hier noch iwie vor warten
-        homeViewModel.getAllFragmentsOfCurrentUebung().observe(this, new Observer<List<com.example.datenbankefuerprojekt.db.main.database.fragment.Fragment>>() {
+        textViewTitle.setText(receivedUebung.getTitel());
+
+
+        //um vor den zugriff auf List<Fragment> fragments sicherzugehen, das es nicht null ist
+        //wird observed und geguckt ob die fragments null sind und ob sie leer sind.
+        //wenn dies nicht der fall ist dann wird begonnen mit dem Zugriff.
+        homeViewModel.getAllFragmentsOfCurrentUebung().observe(getViewLifecycleOwner(), new Observer<List<com.example.datenbankefuerprojekt.db.main.database.fragment.Fragment>>() {
             @Override
             public void onChanged(List<com.example.datenbankefuerprojekt.db.main.database.fragment.Fragment> fragments) {
                 if(fragments != null){
@@ -94,8 +111,24 @@ public class AnimationFragment extends Fragment {
                     }else{
                         mViewModel.setFragmentsOfCurrentUebung(fragments);
                         com.example.datenbankefuerprojekt.db.main.database.fragment.Fragment test = mViewModel.getFragmentsOfCurrentUebung().get(0);
+                        //wenn diese if bedingung True ist, dann ist alles gucci gang
                         if(test.getUebungId() == mViewModel.getCurrentUebung().getId()){
-                            Toast.makeText(getContext(), "Es geht lmaooooooo", Toast.LENGTH_SHORT).show();
+                            Log.i(TAG, "onChanged: dolepe?");
+
+                            Toast.makeText(getContext(), "Bereit", Toast.LENGTH_SHORT).show();
+                            /*Fehler: wenn das Fragment geschlossen und wieder aufgerufen wird, dann wird diese onchanged methode doppelt aufrufen
+                              und das zerstört die animation, sie überspringt die luftAnhaltTeile
+
+                              mit der alreadyInitialised boolean variable wird dieser fall abgefangen!
+                             */
+                            if(!alreadyInitialised) {
+                                mViewModel.initUebung();
+                                timerInitObservers();
+                                setupAnimation();
+                                lottieInitObservers();
+                                setListeners();
+                            }
+                            alreadyInitialised = true;
                         }
                         //init die ganze animations shit!!!
                     }
@@ -108,13 +141,26 @@ public class AnimationFragment extends Fragment {
         return root;
     }
 
+    /*
     @Override
     public void onStop() {
         super.onStop();
         mViewModel.setSavedFrame(lottieAnimationView.getFrame());
-        mViewModel.setPlaying(lottieAnimationView.isAnimating());
+        //mViewModel.setPlaying(lottieAnimationView.isAnimating());
+    }*/
+
+    private void initMemberVariables(){
+        lottieAnimationView = binding.lottiePolygon;
+        buttonStartPause = binding.buttonStartPause;
+        resetButton = binding.resetButton;
+        textViewCountDown = binding.textViewTimer;
+        textViewTitle = binding.textViewAnimationTitle;
+        textViewFragmentTitle = binding.textViewFragmentTitle;
+        //ich brauch noch ne reset methode
     }
 
+
+/*
     private void changeAnimation(){
         //check if it is a certain state
         //move to the next state in the chain
@@ -145,14 +191,13 @@ public class AnimationFragment extends Fragment {
             mViewModel.setState(BreatheAnimationState.breathe_in);
 
             //calcAndSetSpeed(TIME_BREATHE_IN);
-
+            //when it has finished one turn, then increase the repetitions
+            mViewModel.increaseCurrentFragmentRepetitions();
         }
-    }
 
-    /**
-     *
-     * entnimmt den {@link BreatheAnimationState} der animation und setted die passende animation bei {@link LottieAnimationView}
-     * */
+    }*/
+
+    /*
     private void initAnimationView(){
         lottieAnimationView.setSpeed(mViewModel.getSpeed());
         lottieAnimationView.setMinFrame(mViewModel.getSavedFrame());
@@ -164,7 +209,7 @@ public class AnimationFragment extends Fragment {
             //calcAndSetSpeed(TIME_BREATHE_IN);
 
         } else if(mViewModel.getState() == BreatheAnimationState.hold_up){
-            lottieAnimationView.setAnimation(R.raw.hold_up);
+            lottieAnimationView.setAnimation(R.raw.hold_up)
             //calcAndSetSpeed(TIME_HOLD_UP);
 
         } else if(mViewModel.getState() == BreatheAnimationState.breathe_out) {
@@ -172,35 +217,155 @@ public class AnimationFragment extends Fragment {
             //calcAndSetSpeed(TIME_BREATHE_OUT);
 
         } else if (mViewModel.getState() == BreatheAnimationState.hold_down){
-            lottieAnimationView.setAnimation(R.raw.breathe_out);
+            lottieAnimationView.setAnimation(R.raw.hold_down);
             //calcAndSetSpeed(TIME_HOLD_DOWN);
         }
 
-    }
+    }*/
 
-    private void calcAndSetSpeed(float duration){
-        //if 0 then set speed to 10000f so the part of the animation gets skipped
-        float speed = 10000f;
 
-        if(duration != 0f){
-            speed = BASE_ANIMATION_DURATION / duration;
-        }
-
-        mViewModel.setSpeed(speed);
-        lottieAnimationView.setSpeed(speed);
-
-    }
 
 
 
     //hilfsmethoden ab hier
 
-    private void initMemberVariables(){
-        lottieAnimationView = binding.lottiePolygon;
-        startButton = binding.startAnimationButton;
-        stopButton = binding.stopAnimationButton;
-        resetButton = binding.resetButton;
-        //ich brauch noch ne reset methode
+
+
+    /* wird eh bei init Uebung aufgerufen!
+    private void prepareLottieParameters(){
+        mViewModel.calcAndSetSpeed();
+    }*/
+
+
+    //diese observer müssen raus!!!!!!!!!!!!!!
+    private void lottieInitObservers(){
+
+
+        /*
+        //hier könnte es ein problem geben, was wenn er playAnimation macht bevor er eine animation hat?
+        mViewModel.isUebungRunning().observe(getViewLifecycleOwner(), running -> {
+            if(running){
+                lottieAnimationView.setMinFrame(mViewModel.getSavedFrame());
+                lottieAnimationView.playAnimation();
+            } else {
+                lottieAnimationView.pauseAnimation();
+                mViewModel.setSavedFrame(lottieAnimationView.getFrame());
+            }
+        });*/
+        mViewModel.isUebungFinished().observe(getViewLifecycleOwner(), finished -> {
+            lottieAnimationView.pauseAnimation();
+        });
+
+
+    }
+
+    /**
+     * greift auf TimeLeftInMillis und UebungDurationZu, also erst aufrufen wenn diese initialisiert wurden
+     * */
+    private void timerInitObservers(){
+        mViewModel.isUebungRunning().observe(getViewLifecycleOwner(), timerRunning -> {
+            if(timerRunning){
+                buttonStartPause.setText(R.string.pause);
+                resetButton.setEnabled(false);
+            } else {
+                buttonStartPause.setText(R.string.start);
+                //das hier is blöd aber die resources gehen nicht
+                //die antwort is glaub ich ne benutzerdefinierte value!!!
+                if(mViewModel.getTimeLeftInMillis().getValue() != mViewModel.getUebungDurationInSeconds())
+                    resetButton.setEnabled(true);
+
+            }
+        });
+
+
+        mViewModel.isUebungFinished().observe(getViewLifecycleOwner(), timerFinished -> {
+            if(timerFinished){
+                buttonStartPause.setEnabled(false);
+                Log.i(TAG, "onChanged: buttonstartpauseinvvvv");
+            } else {
+                buttonStartPause.setEnabled(true);
+            }
+        });
+
+        mViewModel.getTimeLeftInMillis().observe(getViewLifecycleOwner(), timeLeftInMillis -> {
+            int minutes = (int) (timeLeftInMillis / 1000) / 60; //divide by 1000 for seconds, then by 60 for minutes
+            int seconds = (int) (timeLeftInMillis / 1000) % 60; // modulus for getting the leftover seconds after extracting minutes
+
+            String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d", minutes, seconds);
+
+            textViewCountDown.setText(timeLeftFormatted);
+        });
+    }
+
+    private void setListeners(){
+
+        buttonStartPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mViewModel.isUebungRunning().getValue()){
+                    mViewModel.pauseTimer();
+                    lottieAnimationView.pauseAnimation();
+                    mViewModel.setSavedFrame(lottieAnimationView.getFrame());
+                } else {
+                    mViewModel.startTimer();
+                    lottieAnimationView.setMinFrame(mViewModel.getSavedFrame());
+                    lottieAnimationView.playAnimation();
+                }
+            }
+        });
+
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mViewModel.resetTimer();
+                mViewModel.resetUebung();
+                resetButton.setEnabled(false);
+                lottieAnimationView.setMinFrame(0);
+                lottieAnimationView.setProgress(0);
+                setupAnimation();
+            }
+        });
+
+        resetButton.setEnabled(false);
+        //buttonStartPause.setEnabled(true);
+
+        lottieAnimationView.addAnimatorListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                lottieAnimationView.setMinFrame(0);
+                lottieAnimationView.setProgress(0);
+                //mViewModel.increaseCurrentFragmentRepetitions();
+                mViewModel.changeToNextBreatheAnimationState();
+                mViewModel.calcAndSetSpeed();
+
+                setupAnimation();
+                lottieAnimationView.playAnimation();
+            }
+        });
+    }
+
+    private void setupAnimation(){
+        Log.i(TAG, "setupAnimation: speed:" + mViewModel.getSpeed());
+        Log.i(TAG, "setupAnimation: Progress" + lottieAnimationView.getProgress());
+        lottieAnimationView.setSpeed(mViewModel.getSpeed());
+
+
+        textViewFragmentTitle.setText(mViewModel.getCurrentFragment().getTitelFragment());
+
+        if(mViewModel.getState() == BreatheAnimationState.breathe_in){
+                lottieAnimationView.setAnimation(R.raw.breathe_in);
+        }
+        if(mViewModel.getState() == BreatheAnimationState.hold_up) {
+            lottieAnimationView.setAnimation(R.raw.hold_up);
+        }
+        if(mViewModel.getState() == BreatheAnimationState.breathe_out) {
+            lottieAnimationView.setAnimation(R.raw.breathe_out);
+        }
+        if(mViewModel.getState() == BreatheAnimationState.hold_down){
+            lottieAnimationView.setAnimation(R.raw.hold_down);
+        }
     }
 }
 
--- app/src/main/java/com/example/datenbankefuerprojekt/animation/AnimationViewModel.java
+++ app/src/main/java/com/example/datenbankefuerprojekt/animation/AnimationViewModel.java
@@ -1,5 +1,10 @@
 package com.example.datenbankefuerprojekt.animation;
 
+import android.os.CountDownTimer;
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.datenbankefuerprojekt.db.main.database.fragment.Fragment;
@@ -8,6 +13,7 @@ import com.example.datenbankefuerprojekt.db.main.database.uebung.Uebung;
 import java.util.List;
 
 public class AnimationViewModel extends ViewModel {
+    public static final String TAG = "AnimationViewModel";
 
     /* TODO: iwie durch uebung iterieren
              von test projekt rüberholen
@@ -26,20 +32,214 @@ public class AnimationViewModel extends ViewModel {
 
     private Uebung currentUebung;
 
+
+    //time oder repetitions of uebung
+    private int uebungDurationInSeconds;
+
     private List<Fragment> fragmentsOfCurrentUebung;
     private Fragment currentFragment;
+    private int currentFragmentRepetitions;
+    private int currentFragmentListPosition;
     //hier iwie methode - go to next Fragment
     //da auch arrayindexoutofbounds abfangen unsooooo
 
+
     private int savedFrame;
     private BreatheAnimationState state;
+    //lottie soll das observen
     private float speed;
-    private boolean playing;
 
+    private CountDownTimer countDownTimer;
+
+    private MutableLiveData<Boolean> uebungRunning;
+    private MutableLiveData<Boolean> uebungFinished;
+    private MutableLiveData<Long> timeLeftInMillis;
+
+    /**
+     * Dieser Konstruktor sollte nur aufgerufen werden wenn das Fragment neu geöffnet wird, da das Fragment der Owner des Viewmodels ist
+     * Dieser Aufruf passiert nur wenn der User eine Übung von vorne beginnen möchte, also werden alle werte auf 0 gesetzt
+     *
+     * keine von den Variablen die mit uebung oder Fragment zu tun haben werden hier abgefragt um racecondition
+     * (Konstruktoraufruf vs SQL abfrage der Uebung und fragmente)
+     *
+     * */
     public AnimationViewModel(){
+
+
+        currentFragmentListPosition = 0;
+        currentFragmentRepetitions = 0;
+
         savedFrame = 0;
         state = BreatheAnimationState.breathe_in;
         speed = 4f;
+
+        uebungRunning = new MutableLiveData<>(false);
+        uebungFinished = new MutableLiveData<>(false);
+        //8000 ist ein dummy wert der überschrieben wird, nicht 0 da observer gucken ob dies 0 ist
+        timeLeftInMillis = new MutableLiveData<>(8000L);
+
+    }
+
+    /**
+     * <p>wird aufgerufen wenn currentUebung und fragmentsOfCurrentUebung schon initialisiert sind
+     * </p>
+     *
+     * <p>Berechnung für den fall das wiederholungen verwendet werden: </p>
+     *
+     *  <p>((breatheIn + holdIn + breatheOut + holdOut)*wiederholungenFragment) für alle fragmente</p>
+     *    ergebnis * wiederholungenUebung
+     * */
+    public void initUebung(){
+        if(currentUebung.getUseTimed()){
+            uebungDurationInSeconds = currentUebung.getTimeInSeconds();
+        } else {
+            int durationHelp = 0;
+            for(Fragment f: fragmentsOfCurrentUebung){
+                durationHelp += f.getAnzahlWiederholungenFragment()*(f.getEinAtmenZeit()+f.getEinLuftanhaltZeit()+f.getAusAtmenZeit()+f.getAusLuftanhaltZeit());
+            }
+            uebungDurationInSeconds = durationHelp * currentUebung.getAnzahlDerWiederholungen();
+
+        }
+        resetUebung();
+    }
+
+
+    /**
+     * wird vom Reset button aufgerufen, alles in anfangszustand
+     * */
+    public void resetUebung(){
+        timeLeftInMillis.setValue(uebungDurationInSeconds * 1000L);
+        currentFragmentListPosition = 0;
+        currentFragmentRepetitions = 0;
+        currentFragment = fragmentsOfCurrentUebung.get(currentFragmentListPosition);
+
+
+        savedFrame = 0;
+        state = BreatheAnimationState.breathe_in;
+        calcAndSetSpeed();
+    }
+
+
+
+    /**
+     * erst aufrufen wenn Uebung und Fragmente festgelegt sind
+     * vlt eine initMethode?
+     * */
+    /*
+    private void startUebung(){
+        currentFragmentListPosition = 0;
+        currentFragment = fragmentsOfCurrentUebung.get(currentFragmentListPosition);
+    }*/
+
+    //diese Methode in startUebung aufrufen
+    public void startTimer(){
+        countDownTimer = new CountDownTimer(timeLeftInMillis.getValue(), 1000) {
+            @Override
+            public void onTick(long millisLeftUntilFinished) {
+                timeLeftInMillis.setValue(millisLeftUntilFinished);
+
+            }
+
+            @Override
+            public void onFinish() {
+                Log.i(TAG, "onFinish von timer aufgerufen");
+                uebungRunning.setValue(false);
+                uebungFinished.setValue(true);
+            }
+        }.start();
+        uebungRunning.setValue(true);
+        uebungFinished.setValue(false);
+
+    }
+
+    public void pauseTimer(){
+        countDownTimer.cancel();
+        uebungRunning.setValue(false);
+
+    }
+
+    public void resetTimer(){
+        timeLeftInMillis.setValue((long)uebungDurationInSeconds);
+        uebungFinished.setValue(false);
+    }
+
+    private void changeToNextFragment(){
+
+        if(currentFragmentListPosition+1 < fragmentsOfCurrentUebung.size()){
+            currentFragmentListPosition++;
+
+        } else {
+            //ist Uebung duration schon vorbei oder die anzahl an wiederholungen schon durch? dann ende
+            // das wird im timer abgefragt, also hier nicht!
+
+            currentFragmentListPosition = 0;
+        }
+        currentFragment = fragmentsOfCurrentUebung.get(currentFragmentListPosition);
+
+    }
+
+    public void increaseCurrentFragmentRepetitions(){
+        if(currentFragmentRepetitions+1 < currentFragment.getAnzahlWiederholungenFragment()){
+            currentFragmentRepetitions++;
+        }else{
+            currentFragmentRepetitions = 0;
+            changeToNextFragment();
+        }
+    }
+
+
+
+
+    //könnte raceconditions hevorrufen
+    public void changeToNextBreatheAnimationState(){
+
+        if(state == BreatheAnimationState.breathe_in){
+            Log.i(TAG, "changeToNextBreatheAnimationState: breathe in ");
+            state = BreatheAnimationState.hold_up;
+
+        }else if(state == BreatheAnimationState.hold_up) {
+            Log.i(TAG, "changeToNextBreatheAnimationState: holdup ");
+            state = BreatheAnimationState.breathe_out;
+
+        }else if(state == BreatheAnimationState.breathe_out) {
+            Log.i(TAG, "changeToNextBreatheAnimationState: breathe out ");
+            state = BreatheAnimationState.hold_down;
+
+        }else if(state == BreatheAnimationState.hold_down){
+            state = BreatheAnimationState.breathe_in;
+            increaseCurrentFragmentRepetitions();
+            Log.i(TAG, "changeToNextBreatheAnimationState: holddown ");
+
+        }
+    }
+
+
+    /**
+     * currentFragment wird abgerufen, also erst nach dem diese Aus datenbank gekommen sind aufrufen
+     * */
+    public void calcAndSetSpeed(){
+        float duration = 0f;
+        if(state == BreatheAnimationState.breathe_in){
+            duration = (float) currentFragment.getEinAtmenZeit();
+        }
+        if(state == BreatheAnimationState.hold_up) {
+            duration = (float) currentFragment.getEinLuftanhaltZeit();
+
+        }
+        if(state == BreatheAnimationState.breathe_out) {
+            duration = (float) currentFragment.getAusAtmenZeit();
+
+        }
+        if(state == BreatheAnimationState.hold_down){
+            duration = (float) currentFragment.getAusLuftanhaltZeit();
+
+        }
+        //if 0 then set speed to 10000f so the part of the animation gets skipped
+        if(duration == 0f) {
+            this.speed = 10000f;
+        }else{
+            this.speed = BASE_ANIMATION_DURATION / duration;
+        }
     }
 
     public Uebung getCurrentUebung() {
@@ -62,24 +262,8 @@ public class AnimationViewModel extends ViewModel {
         return state;
     }
 
-    public void setState(BreatheAnimationState state) {
-        this.state = state;
-    }
-
     public float getSpeed() {
         return speed;
-    }
-
-    public void setSpeed(float speed) {
-        this.speed = speed;
-    }
-
-    public boolean isPlaying() {
-        return playing;
-    }
-
-    public void setPlaying(boolean playing) {
-        this.playing = playing;
     }
 
     public List<Fragment> getFragmentsOfCurrentUebung() {
@@ -88,5 +272,23 @@ public class AnimationViewModel extends ViewModel {
 
     public void setFragmentsOfCurrentUebung(List<Fragment> fragmentsOfCurrentUebung) {
         this.fragmentsOfCurrentUebung = fragmentsOfCurrentUebung;
+    }
+
+    public Fragment getCurrentFragment() {
+        return currentFragment;
+    }
+
+    public int getUebungDurationInSeconds() {
+        return uebungDurationInSeconds;
+    }
+
+    public LiveData<Boolean> isUebungRunning() {
+        return uebungRunning;
+    }
+    public LiveData<Boolean> isUebungFinished() {
+        return uebungFinished;
+    }
+    public LiveData<Long> getTimeLeftInMillis(){
+        return timeLeftInMillis;
     }
 }
